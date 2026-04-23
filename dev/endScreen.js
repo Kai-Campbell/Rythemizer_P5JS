58e4474225d483015e63a70146d8463aa0a6d1ce
Kai-Campbell <kaimcampbell2002@gmail.com>
2026-04-23 18:13:01 -0400

    Adjusted the end screen to stop on the right spot

--- dev/endScreen.js
+++ dev/endScreen.js
@@ -3,19 +3,19 @@ let walkingDone = false;
 
 function endScreenSetup() {
   gifX = -80;                          
-  gifY = (CANVAS_HEIGHT / 2) + 80;    
+  gifY = (CANVAS_HEIGHT / 2) + 155;    
   walkingDone = false;
 }
 
 function endScreenDraw() {
   if (!walkingDone) {
     image(endScene, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    if (gifX < CANVAS_WIDTH / 2) {
+    if (gifX < (CANVAS_WIDTH / 2) - 30) {
       gifX += 4;
     } else {
       walkingDone = true;
     }
-    image(playerWalking, gifX, gifY, 80, 80); 
+    image(playerWalking, gifX, gifY, 105, 105); 
   } else {
     image(endScenePlayer, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
   }
